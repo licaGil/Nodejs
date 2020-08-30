121613e548d411abe3fbfebdfc8a3b8b37cf64b0
user.name like: "LicaGil <user.email like: "lica.gil.79@gmail.com>
2020-08-30 15:10:54 +0300

    --- Auto-Git Commit ---

--- 001/app.js
+++ 001/app.js
@@ -1,8 +1,16 @@
 const http = require('http');
+const os = require('os');
+
+console.log(os);
+
+
+
 
 http = createServer((req, resp) => {
 
-}).listen(3000, "127.0.0.1", () => {
+    resp.end('Hello world!');
+
+}).listen(5500, "127.0.0.1", () => {
 
     console.log('Success start!');
 });
